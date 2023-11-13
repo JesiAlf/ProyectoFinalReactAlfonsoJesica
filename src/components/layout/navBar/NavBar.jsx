5c91c5fe48760fc508af0503bc8808766a6324e0
Jesica Alfonso <jesica.l.alfonso@gmail.com>
2023-11-13 11:41:53 -0300

    ProyectoFinalReactAlfonsoJesica

--- src/components/layout/navBar/NavBar.jsx
+++ src/components/layout/navBar/NavBar.jsx
@@ -1,5 +1,5 @@
 
-import CartWidget from "../../common/CartWidget/CartWidget";
+import CartWidget from "../../common/cartWidget/CartWidget";
 import "./Navbar.css";
 import {Link, Outlet} from "react-router-dom";
 
